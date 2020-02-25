04eb09e0a64ae9e971a58f8fc3eda397aad1b60a
david-forer <david.j.forer@gmail.com>
2020-02-25 20:31:42 +0900

    Generate product model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ has_secure_password
     validates :email, uniqueness: true
     validates_format_of :email, with: /@/
     validates :password_digest, presence: true
+
+    has_many :products, dependent: :destroy
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -22,5 +22,11 @@ class UserTest < ActiveSupport::TestCase
     'test')
     assert_not user.valid?
   end
+# Destroy products when user is destroyed
+  test 'destroy user should destroy linked product' do
+    assert_difference('Product.count', -1) do
+    users(:one).destroy
+  end
+end
 
 end
